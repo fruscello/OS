17c75dd3062e5382c30e89032b10e1a1e5659e6e
fruscello <34569702+fruscello@users.noreply.github.com>
2018-09-22 14:27:35 +0200

    Update syscall.h

--- syscall.h
+++ syscall.h
@@ -1,8 +1,19 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+#include "./asl/asl.h"
+#include "./pcb/pcb_t.h"
+#include "./pcb/const.h"
+#include "/usr/include/uarm/uARMtypes.h"
+state_t *sys5_sys_old;
+state_t *sys5_TLB_old;
+state_t *sys5_PGMT_old;
+state_t *sys5_sys_new;
+state_t *sys5_TLB_new;
+state_t *sys5_PGMT_new;
+pcb_t* pcb_wait;
 void P(int *semaddr);
-void P(int *semaddr);
-CREATEPROCESS(state_t *statep,int priority,void **cpid);
+void V(int *semaddr);
+int CREATEPROCESS(state_t *statep,int priority,void **cpid);
 int TERMINATEPROCESS(void *pid);
 #endif
